e16221f38e4e79d6752c6eb05654ed7f1f315164
alitarika <130107495+alitarika@users.noreply.github.com>
2023-04-08 17:27:20 +0200

    Create testchildbranch.py

---
+++ testchildbranch.py
@@ -0,0 +1,4 @@
+# Testing the basics of github repositories by messing around to find around.
+# Adding a new file in child branch as testchildbranch.py.
+print("This .py is within the testchildbranch.")
+
