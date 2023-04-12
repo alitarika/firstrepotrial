d6b044734f28364f0a1dbc922aee8777eceeab57
alitarika <alitarikalacam@gmail.com>
2023-04-12 03:08:22 +0200

    213413

--- testchildbranch.py
+++ testchildbranch.py
@@ -1,4 +1,4 @@
 # Testing the basics of github repositories by messing around to find out.
 # Adding a new file in child branch as testchildbranch.py.
 print("This .py is within the testchildbranch.")
-
+print("cloned using visual code. now I wonder if I can push the new code.")
